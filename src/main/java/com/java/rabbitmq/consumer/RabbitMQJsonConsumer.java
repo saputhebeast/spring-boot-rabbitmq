e4d4e3e3ce6af844afb5d88bf63ea9239961a608
saputhebeast <sapumalwijekoon4@gmail.com>
2024-01-28 20:07:48 +0530

    Feat: json consumer

---
+++ src/main/java/com/java/rabbitmq/consumer/RabbitMQJsonConsumer.java
@@ -0,0 +1,18 @@
+package com.java.rabbitmq.consumer;
+
+import com.java.rabbitmq.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQJsonConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJsonConsumer.class);
+
+    @RabbitListener(queues = {"${rabbitmq.queue.json_name}"})
+    public void jsonMessageConsumer(User user) {
+        LOGGER.info(String.format("Received JSON message -> %s", user.toString()));
+    }
+}
