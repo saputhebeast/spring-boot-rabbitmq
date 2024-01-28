2363650a4f1ffd5aa91f4f4bdfbd07cea2d98344
saputhebeast <sapumalwijekoon4@gmail.com>
2024-01-28 20:08:47 +0530

    Feat: json controller

---
+++ src/main/java/com/java/rabbitmq/controller/MessageJsonController.java
@@ -0,0 +1,26 @@
+package com.java.rabbitmq.controller;
+
+import com.java.rabbitmq.dto.User;
+import com.java.rabbitmq.producer.RabbitMQJsonProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageJsonController {
+
+    private final RabbitMQJsonProducer rabbitMQJsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer rabbitMQJsonProducer) {
+        this.rabbitMQJsonProducer = rabbitMQJsonProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user) {
+        rabbitMQJsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("Json message sent to RabbitMQ...");
+    }
+}
